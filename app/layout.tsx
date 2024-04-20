d3c7941c564f82ee6deb6885354b06ca54347c4b
kolade <chrisjay967@gmail.com>
2024-04-20 14:00:03 +0100

    removed comments on layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,10 @@ import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 
 export const metadata = {
-  // newly added code
-  title: 'Green World | Home of Nature Scenes', // newly added code
-  description: 'Find the coolest nature images and videos', // newly added code
-  keywords: 'nature animals trees fields', // newly added code
-}; // newly added code
+  title: 'Green World | Home of Nature Scenes',
+  description: 'Find the coolest nature images and videos',
+  keywords: 'nature animals trees fields',
+};
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
